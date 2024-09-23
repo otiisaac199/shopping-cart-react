b7cc1db0f222a4c983823bb691d7bdcbbd379abe
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-23 10:27:23 +0100

    feat: mapped the products data and returned the value

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,16 @@ import iconCart from "../assets/images/iconCart.png";
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between items-center mb-5">
       <Link to="/" className="text-xl font-semibold">
         Home.
       </Link>
+      <div className="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center relative ">
+        <img src={iconCart} alt="" className="w-6" />
+        <span className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center ">
+          0
+        </span>
+      </div>
     </header>
   );
 };
