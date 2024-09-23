6e21756b3193b965bc1d3fc0d1b6deffdb2c4e49
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-23 11:56:36 +0100

    feat: added the products and detail for each product to a particular route

---
+++ src/store/cart.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const { productId, quantity } = action.payload;
+      const indexProductId = state.items.findIndex(
+        (item) => item.productId === productId
+      );
+      if (indexProductId >= 0) state.items[indexProductId].quantity += quantity;
+      else state.items.push({ productId, quantity });
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
